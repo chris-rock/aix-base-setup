aafdf1490b68d03471a8b0f3ee3c835f5084985e
Christoph Hartmann <chris@lollyrock.com>
2015-01-20 10:37:53 +0100

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'aix-base-setup'
+maintainer       'Christoph Hartmann'
+maintainer_email 'chris@lollyrock.com'
+license          'Apache license 2.0'
+description      'Configures AIX with basic cli tools'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'aix'
